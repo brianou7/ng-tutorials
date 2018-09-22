fd9dceabf30a0df7e5ea65f9112877ca5f9e6b6e
Brian <brahianocampo7@gmail.com>
2018-09-22 00:30:32 -0500

    Body and footer component finished to my angular's hello world app

--- 01-hello-world/src/app/components/body/body.component.ts
+++ 01-hello-world/src/app/components/body/body.component.ts
@@ -4,4 +4,15 @@ import { Component } from '@angular/core';
     selector: 'app-body',
     templateUrl: './body.component.html'
 })
-export class BodyComponent {}
+export class BodyComponent {
+
+    show = true;
+    phrase: any = {
+        text: 'With great power comes great responsability',
+        author: 'Ben Parker'
+    };
+
+    characters: String[] = [
+        'Spiderman', 'Venon', 'Dr. Octopus'
+    ];
+}
